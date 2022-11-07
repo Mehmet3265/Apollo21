a815ffc0a04ad41582bfe1c5a1bba4bf058ab472
Mehmet3265 <mehmetfurkangulgor@gmail.com>
2022-11-07 22:54:34 +0300

    project is created

---
+++ src/test/java/runners/CukesRunner.java
@@ -0,0 +1,31 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target.rerun.txt",
+                "json:target/cucumber.json",
+                "junit:target/junit/junit-report.xml",
+                "me.jvt.cucumber.report.PrettyReports:target.cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definations",
+        dryRun = false,
+        tags = "@BRT-764",
+        publish = true
+)
+
+public class CukesRunner {
+}
+
+
+
+
+
